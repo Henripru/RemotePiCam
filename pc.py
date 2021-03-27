d2f236501f30c23a4bf61f5ed30b097d598191d9
Henri Prudhomme <henripru99@gmail.com>
2021-03-26 20:56:42 -0400

    add files

---
+++ pc.py
@@ -0,0 +1,47 @@
+# CS326 MQTT Lab
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+from PIL import Image
+import time
+
+# Constants
+BROKER = 'iot.cs.calvin.edu'
+PORT = 1883
+QOS = 0
+USERNAME = 'cs326' # broker username (if required)
+PASSWORD = 'piot' # broker password (if required)
+MAIN_TOPIC = 'RPC' # topic to publish under
+
+# Callback when a connection has been established with the MQTT broker
+def on_connect(client, userdata, rc, *extra_params):
+    print('Connected with result code={}'.format(rc))
+
+# Callback when client receives a message from the broker
+# Use button message to turn LED on/off
+def on_message(client, data, msg):
+    if msg.topic == "RPC/image":
+        # Create a file with write byte permission
+        f = open('./tmp/image.jpg', "wb")
+        f.write(msg.payload)
+        print("Image Received")
+        f.close()
+        image = Image.open('image.jpg')
+        image.show()
+        time.sleep(5)
+        image.close()
+
+# Setup MQTT client and callbacks
+client = mqtt.Client()
+client.username_pw_set(USERNAME, password=PASSWORD)
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect to MQTT broker and subscribe to the button topic
+client.connect(BROKER, PORT, 60)
+client.subscribe("RPC/image", qos=QOS)
+
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    client.disconnect()
+    print("Done")
---
+++ pi.py
@@ -0,0 +1,45 @@
+# CS326 MQTT Lab
+import os
+import paho.mqtt.client as mqtt
+from picamera import PiCamera
+
+# Constants
+BROKER = 'iot.cs.calvin.edu' # CS MQTT broker
+PORT = 1883
+QOS = 0
+USERNAME = 'cs326' # broker username (if required)
+PASSWORD = 'piot' # broker password (if required)
+MAIN_TOPIC = 'RPC'
+
+# MQTT connection callback
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print('Connected to',BROKER)
+    else:
+        print('Connection to {} failed. Return code={}'.format(BROKER,rc))
+        os._exit(1)
+
+
+# Setup MQTT client and callbacks
+client = mqtt.Client()
+client.username_pw_set(USERNAME,password=PASSWORD) # remove for anonymous access
+client.on_connect=on_connect
+client.connect(BROKER, PORT, 60)
+
+# Connect to camera
+camera = PiCamera()
+
+# Main Loop
+try:
+    camera.start_preview()
+    while True:
+        client.loop(timeout=5.0)
+        camera.capture('/tmp/image.jpg')
+        f = open("/tmp/image.jpg", "rb")
+        fileContent = f.read()
+        byteArr = bytearray(fileContent)
+        client.publish("RPC/image", byteArr, qos=QOS)
+except KeyboardInterrupt:
+    client.disconnect()
+    camera.stop_preview()
+    print("Done")
