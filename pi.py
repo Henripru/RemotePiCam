7046fe3d2fe6b56d8eef3b3b09e50de75845f76f
Braden Weber <bradenlweber@gmail.com>
2021-03-27 11:50:53 -0400

    update

--- pi.py
+++ pi.py
@@ -3,6 +3,7 @@ import os
 import paho.mqtt.client as mqtt
 from picamera import PiCamera
 import time
+import pigpio
 
 # Constants
 BROKER = 'iot.cs.calvin.edu' # CS MQTT broker
@@ -11,6 +12,43 @@ QOS = 0
 USERNAME = 'cs326' # broker username (if required)
 PASSWORD = 'piot' # broker password (if required)
 MAIN_TOPIC = 'RPC'
+SERVOX = 12
+SERVOY = 13
+
+pi = pigpio.pi()
+if not pi.connected():
+    exit(0)
+
+pi.set_PWM_frequency(SERVOX, 50)
+pi.set_PWM_frequency(SERVOY, 50)
+
+current_angle_x = 0
+current_angle_y = 0
+
+pi.set_servo_pulsewidth(SERVOX, (400/72)*(current_angle_x) + 1500)
+pi.set_servo_pulsewidth(SERVOY, (400/72)*(current_angle_y) + 1500)
+
+# ... receive delta x and y
+
+def set_x(current_angle_x):
+    if current_angle_x + delta_x > 80:
+        current_angle_x = 80
+    elif current_angle_x + delta_x < -80:
+        current_angle_x = -80
+    else:
+        current_angle_x += delta_x
+    pi.set_servo_pulsewidth(SERVOX, (400/72)*(current_angle_x) + 1500)
+    return current_angle_x
+
+def set_y(current_angle_y):
+    if current_angle_y + delta_y > 80:
+        current_angle_y = 80
+    elif current_angle_y + delta_y < -80:
+        current_angle_y = -80
+    else:
+        current_angle_y += delta_y
+    pi.set_servo_pulsewidth(SERVOY, (400/72)*(current_angle_y) + 1500)
+    return current_angle_y
 
 # MQTT connection callback
 def on_connect(client, userdata, flags, rc):
@@ -20,12 +58,18 @@ def on_connect(client, userdata, flags, rc):
         print('Connection to {} failed. Return code={}'.format(BROKER,rc))
         os._exit(1)
 
+def on_message(client, data, msg):
+    if msg.topic == "RPC/position":
+        string1 = msg.payload
+        print(string1)
 
 # Setup MQTT client and callbacks
 client = mqtt.Client()
 client.username_pw_set(USERNAME,password=PASSWORD) # remove for anonymous access
 client.on_connect=on_connect
+client.on_message=on_message
 client.connect(BROKER, PORT, 60)
+client.subscribe("RPC/position", qos = QOS)
 
 # Connect to camera
 camera = PiCamera()
@@ -41,6 +85,8 @@ try:
         fileContent = f.read()
         byteArr = bytearray(fileContent)
         client.publish("RPC/image", byteArr, qos=QOS)
+
+
 except KeyboardInterrupt:
     client.disconnect()
     camera.stop_preview()
