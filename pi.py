a35821118ab406881e84b3968725f182528e311e
Henri Prudhomme <henripru99@gmail.com>
2021-03-27 12:21:20 -0400

    update

--- pi.py
+++ pi.py
@@ -16,7 +16,7 @@ SERVOX = 12
 SERVOY = 13
 
 pi = pigpio.pi()
-if not pi.connected():
+if not pi.connected:
     exit(0)
 
 pi.set_PWM_frequency(SERVOX, 50)
@@ -30,24 +30,24 @@ pi.set_servo_pulsewidth(SERVOY, (400/72)*(current_angle_y) + 1500)
 
 # ... receive delta x and y
 
-def set_x(current_angle_x):
+def set_x(current_angle_x, delta_x):
     if current_angle_x + delta_x > 80:
         current_angle_x = 80
     elif current_angle_x + delta_x < -80:
         current_angle_x = -80
     else:
         current_angle_x += delta_x
-    pi.set_servo_pulsewidth(SERVOX, (400/72)*(current_angle_x) + 1500)
+    pi.set_servo_pulsewidth(SERVOX, int((400/72)*(current_angle_x) + 1500))
     return current_angle_x
 
-def set_y(current_angle_y):
+def set_y(current_angle_y, delta_y):
     if current_angle_y + delta_y > 80:
         current_angle_y = 80
     elif current_angle_y + delta_y < -80:
         current_angle_y = -80
     else:
         current_angle_y += delta_y
-    pi.set_servo_pulsewidth(SERVOY, (400/72)*(current_angle_y) + 1500)
+    pi.set_servo_pulsewidth(SERVOY, int((400/72)*(current_angle_y) + 1500))
     return current_angle_y
 
 # MQTT connection callback
@@ -59,9 +59,12 @@ def on_connect(client, userdata, flags, rc):
         os._exit(1)
 
 def on_message(client, data, msg):
+    global current_angle_x
+    global current_angle_y
     if msg.topic == "RPC/position":
-        string1 = msg.payload
-        print(string1)
+        stringtuple = msg.payload.decode().split(',')
+        current_angle_x = set_x(current_angle_x, float(stringtuple[0]))
+        current_angle_y = set_y(current_angle_y, float(stringtuple[1]))
 
 # Setup MQTT client and callbacks
 client = mqtt.Client()
