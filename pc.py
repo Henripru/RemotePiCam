bfd835388cf8784d730e930eec61d4f228b5acc4
Braden Weber <bradenlweber@gmail.com>
2021-03-26 23:54:46 -0400

    update

--- pc.py
+++ pc.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 from PIL import Image
 import time
+import cv2 as cv
 
 # Constants
 BROKER = 'iot.cs.calvin.edu'
@@ -23,9 +24,15 @@ def on_message(client, data, msg):
         f.write(msg.payload)
         print("Image Received")
         f.close()
-        image = Image.open('image.jpg')
-        image.show()
-        image.close()
+        original_image = cv.imread('image.jpg')
+        grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+        face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+        detected_faces = face_cascade.detectMultiScale(grayscale_image)
+        if len(detected_faces) > 0:
+            face_to_track = detected_faces[0]
+            x = face_to_track[0] + face_to_track[2]/2
+            y = face_to_track[1] + face_to_track[3]/2
+            client.publish("RPC/position", str(x) + ',' + str(y), qos = QOS)
 
 # Setup MQTT client and callbacks
 client = mqtt.Client()
